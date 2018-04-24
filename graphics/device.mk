c2b9482d96f583665ec62c0f52638eba6ceb4df0
Yousong Zhou <yszhou4tech@gmail.com>
2018-04-24 17:58:07 +0800

    graphics: switch to swiftshader for non-arm64 devices
    libGLES_android.so is now rejected by SurfaceFlinger which requires at
    least OpenGL ES 2.0.  Swiftshader at the moment does not support arm64

--- graphics/device.mk
+++ graphics/device.mk
@@ -1,7 +1,15 @@
 PRODUCT_PROPERTY_OVERRIDES := $(if $(CONFIG_SW_GRAPHICS), \
 	ro.kernel.qemu=1)
 
-PRODUCT_PACKAGES := libGLES_android
+ifeq ($(TARGET_ARCH),arm64)
+  PRODUCT_PACKAGES := \
+	libGLES_android
+else
+  PRODUCT_PACKAGES := \
+	libEGL_swiftshader \
+	libGLESv1_CM_swiftshader \
+	libGLESv2_swiftshader
+endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
         ro.sf.lcd_density=$(CONFIG_DISPLAY_DPI)
